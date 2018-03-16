4304881e05e17853140e04dc9c06dadc9daedc3e
Aiko419 <35924978+Aiko419@users.noreply.github.com>
2018-03-16 20:42:09 +0700

    Initial commit

---
+++ thuchanh1/thuchanh1/Controllers/BookController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using thuchanh1.Models;
+
+namespace thuchanh1.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public string HelloBookStore(string university)
+        {
+            return "Welcome BookStore! "+ university;
+        }
+
+        public ActionResult ListBooḳ()
+        {
+            var books = new List<string>();
+            books.Add("ASP.NET MVC");
+            books.Add("PHP & MySQL");
+            books.Add("Java");
+            ViewBag.Books = books;
+            return View();
+
+        }
+
+        public ActionResult ListBooḳModel()
+        {
+
+            var books = new List<Book>();
+            books.Add(new Book(1, "ASP.NET MVC", "Nguyễn Văn A", "/Content/Images/book1.jpg"));
+            books.Add(new Book(2, "PHP & MySQL", "Lê Văn Tòng", "/Content/Images/book2.jpg"));
+            books.Add(new Book(3, "Java", "Nguyễn Phúc Hậu", "/Content/Images/book3.jpg"));
+            return View(books);
+
+        }
+
+
+    }
+}
---
+++ thuchanh1/thuchanh1/Models/Book.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace thuchanh1.Models
+{
+    public class Book
+    {
+        private int id;
+        private string title;
+        private string author;
+        private string image_cover;
+
+        public Book()
+        {
+
+        }
+
+        public Book(int id, string title, string author, string image_cover)
+        {
+            this.id = id;
+            this.title = title;
+            this.author = author;
+            this.image_cover = image_cover;
+        }
+
+        public int ID
+        {
+            get { return id; }
+        }
+
+        public string TITLE
+        {
+            get { return title; }
+        }
+
+        public string AUTHOR
+        {
+            get { return author; }
+        }
+
+        public string IMAGE_COVER
+        {
+            get { return image_cover; }
+        }
+
+    }
+}
