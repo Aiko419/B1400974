c236fdc25aeff79383417f8b89bc4e5317d45f23
Aiko419 <35924978+Aiko419@users.noreply.github.com>
2018-03-23 18:36:32 +0700

    Buổi 2
    phương thức thêm sữa sách

--- thuchanh1/thuchanh1/Controllers/BookController.cs
+++ thuchanh1/thuchanh1/Controllers/BookController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using thuchanh1.Models;
-
 namespace thuchanh1.Controllers
 {
     public class BookController : Controller
@@ -12,7 +11,7 @@ namespace thuchanh1.Controllers
         // GET: Book
         public string HelloBookStore(string university)
         {
-            return "Welcome BookStore! "+ university;
+            return "Welcome BookStore! " + university;
         }
 
         public ActionResult ListBooḳ()
@@ -37,6 +36,87 @@ namespace thuchanh1.Controllers
 
         }
 
+        public ActionResult EditBooḳ(int id)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "ASP.NET MVC", "Nguyễn Văn A", "/Content/Images/book1.png"));
+            books.Add(new Book(2, "PHP & MySQL", "Lê Văn Tòng", "/Content/Images/book2.png"));
+            books.Add(new Book(3, "Java", "Nguyễn Phúc Hậu", "/Content/Images/book3.png"));
+            Book book = new Book();
+            foreach (Book b in books)
+            {
+                if (b.ID == id)
+                {
+                    book = b;
+                    break;
+                }
+            }
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
 
+        //Post: Book/EditBook/Id
+        [HttpPost, ActionName("EditBook")] //Dữ liệu được post từ Client
+        [ValidateAntiForgeryToken] //Security help
+        public ActionResult EditBooḳSave(int id, string Title, string Author, string Image_cover)
+        {
+            //Dữ liệu sách ban đầu
+            var books = new List<Book>();
+            books.Add(new Book(1, "ASP.NET MVC", "Nguyễn Văn A", "/Content/Images/book1.png"));
+            books.Add(new Book(2, "PHP & MySQL", "Lê Văn Tòng", "/Content/Images/book2.png"));
+            books.Add(new Book(3, "Java", "Nguyễn Phúc Hậu", "/Content/Images/book3.png"));
+
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            //Lặp tìm cuốn sách theo id và gán giá trị cập nhật
+            foreach (Book b in books)
+            {
+                if (b.ID == id)
+                {
+                    b.TITLE = Title;
+                    b.AUTHOR = Author;
+                    b.IMAGE_COVER = Image_cover;
+                    break;
+                }
+            }
+            //Trả về wiew "ListBookModel" kèm theo đối tượng books, sau khi cập nhật
+            return View("ListBookModel", books);
+        }
+
+        public ActionResult CreateBook()
+        {
+            return View();
+        }
+
+        //Hàm thêm sách mới
+        [HttpPost, ActionName("CreateBook")] //Dữ liệu được post từ Client
+        [ValidateAntiForgeryToken] //Security help
+        public ActionResult CreateBook([Bind(Include = "Id, Title, Author, ImageCover")]Book book)
+        {
+            //Dữ liệu sách ban đầu
+            var books = new List<Book>();
+            books.Add(new Book(1, "ASP.NET MVC", "Nguyễn Văn A", "/Content/Images/book1.png"));
+            books.Add(new Book(2, "PHP & MySQL", "Lê Văn Tòng", "/Content/Images/book2.png"));
+            books.Add(new Book(3, "Java", "Nguyễn Phúc Hậu", "/Content/Images/book3.png"));
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    //Thêm mới sách
+                    books.Add(book);
+                }
+            }
+            catch (Exception /*dex*/)
+            {
+                ModelState.AddModelError("", "Error save data");
+            }
+            return View("ListBookModel", books);
+
+        }
     }
 }
--- thuchanh1/thuchanh1/Models/Book.cs
+++ thuchanh1/thuchanh1/Models/Book.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace thuchanh1.Models
 {
@@ -28,22 +29,30 @@ namespace thuchanh1.Models
         public int ID
         {
             get { return id; }
+            set { id = value; }
         }
-
+        [Required(ErrorMessage = "Tiêu đề không được trống")]
+        [StringLength(250, ErrorMessage = "Tiêu đề sách không vượt quá 250 ký tự")]
         public string TITLE
         {
+            set { title = value; }
             get { return title; }
         }
 
         public string AUTHOR
         {
+            set { author = value; }
             get { return author; }
         }
 
         public string IMAGE_COVER
         {
+            set { image_cover = value; }
             get { return image_cover; }
         }
+
+        
+
 
     }
 }
